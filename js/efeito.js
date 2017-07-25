0f3a5fff3ba4e2ee8ba92c140ff94b07c3089358
LuanPeixotop <luanpeixotopereira@gmail.com>
2017-07-25 15:08:03 -0300

    Suavização do efeito FadeIn e pequenas modificações no HTML

--- js/efeito.js
+++ js/efeito.js
@@ -8,26 +8,26 @@ jQuery('#cardMembros4').fadeOut();
 jQuery(window).scroll(function () {
 
     if (jQuery(this).scrollTop() > 250) {
-        jQuery('#cardExp').fadeIn(1300);
+        jQuery('#cardExp').fadeIn(1400);
     }
 
-    if (jQuery(this).scrollTop() > 860) {
-        jQuery('#cardMentores').fadeIn(1300);
+    if (jQuery(this).scrollTop() > 970) {
+        jQuery('#cardMentores').fadeIn(1400);
     }
 
-    if (jQuery(this).scrollTop() > 1375) {
-        jQuery('#cardMembros1').fadeIn(1300);
+    if (jQuery(this).scrollTop() > 1580) {
+        jQuery('#cardMembros1').fadeIn(1400);
     }
 
-    if (jQuery(this).scrollTop() > 1830) {
-        jQuery('#cardMembros2').fadeIn(1300);
+    if (jQuery(this).scrollTop() > 2100) {
+        jQuery('#cardMembros2').fadeIn(1400);
     }
 
-    if (jQuery(this).scrollTop() > 2375) {
-        jQuery('#cardMembros3').fadeIn(1300);
+    if (jQuery(this).scrollTop() > 2615) {
+        jQuery('#cardMembros3').fadeIn(1400);
     }
 
-    if (jQuery(this).scrollTop() > 2840) {
-        jQuery('#cardMembros4').fadeIn(1300);
+    if (jQuery(this).scrollTop() > 3060) {
+        jQuery('#cardMembros4').fadeIn(1400);
     }
 })
