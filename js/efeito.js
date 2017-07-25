37da1d977f1157457b2c45184c5e33509b256f32
LuanPeixotop <luanpeixotopereira@gmail.com>
2017-07-24 21:57:02 -0300

    Adicionando efeito Card-Reveal e realizando diversas melhores e correções

--- js/efeito.js
+++ js/efeito.js
@@ -5,29 +5,29 @@ jQuery('#cardMembros2').fadeOut();
 jQuery('#cardMembros3').fadeOut();
 jQuery('#cardMembros4').fadeOut();
 
-jQuery(window).scroll(function() {
+jQuery(window).scroll(function () {
 
-    if (jQuery(this).scrollTop()>250) {
+    if (jQuery(this).scrollTop() > 250) {
         jQuery('#cardExp').fadeIn(1300);
     }
-     
-    if (jQuery(this).scrollTop()>850) {
+
+    if (jQuery(this).scrollTop() > 860) {
         jQuery('#cardMentores').fadeIn(1300);
     }
 
-    if (jQuery(this).scrollTop()>1480) {
+    if (jQuery(this).scrollTop() > 1375) {
         jQuery('#cardMembros1').fadeIn(1300);
     }
 
-    if (jQuery(this).scrollTop()>1930) {
+    if (jQuery(this).scrollTop() > 1830) {
         jQuery('#cardMembros2').fadeIn(1300);
     }
 
-    if (jQuery(this).scrollTop()>2405) {
+    if (jQuery(this).scrollTop() > 2375) {
         jQuery('#cardMembros3').fadeIn(1300);
     }
 
-    if (jQuery(this).scrollTop()>2850) {
+    if (jQuery(this).scrollTop() > 2840) {
         jQuery('#cardMembros4').fadeIn(1300);
     }
- })
+})
